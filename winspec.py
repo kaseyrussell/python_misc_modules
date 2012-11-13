be569f8fb5506a8fdf336f9a24be60c31e73651c
Kasey Russell <kasey.russell@gmail.com>
2012-11-13 17:40:18 -0500

    credits updated to include SYC!

--- winspec.py
+++ winspec.py
@@ -16,7 +16,9 @@
 # May 2010:
 # Extended to handle spectroscopy data (x-calibration) and 
 # Spectrum class added for convenience.
-# Kasey Russell (krussell@post.harvard.edu)
+# Kasey Russell (krussell at post.harvard.edu)
+#
+# Some yoffset features added by Shanying Cui
 #
 
 from __future__ import division
@@ -40,8 +42,8 @@ class Spectrum():
     normalizing it [*self.normalize()*], etc.
     """
     def __init__( self, fname, cts_per_sec=False ):
-        if not fname.endswith('.SPE'):
-            raise ValueError( 'Can only create a Spectrum class object from an SPE file; fname=%s' % fname )
+        if not (fname.endswith('.SPE') or fname.endswith('.txt')):
+            raise ValueError( 'Can only create a Spectrum class object from an SPE or txt file; fname=%s' % fname )
             
         self.fname          = fname
         self.cts_per_sec    = cts_per_sec
@@ -52,7 +54,10 @@ class Spectrum():
         self._slope_remove  = False
         self._counts_per_second = False
 
-        self.read_spe( cts_per_sec )
+        if fname.endswith('.SPE'):
+            self.read_spe( cts_per_sec )
+        if fname.endswith('.txt'):
+            self.read_txt()
         self.raw_lum = self.lum.copy()
 
     def background_correct( self, fname  ):
@@ -632,7 +637,16 @@ class Spectrum():
             p = numpy.array([ pcoeffs [2], pcoeffs [1], pcoeffs [0] ])
             self.wavelen = numpy.polyval( p, xrange( 1, 1+len(self.lum) ) )
 
-
+    def read_txt( self ):
+        """
+        Shanying 2/22/2012, reads simple text files with only 2 columns: wavelength and intensity
+        """
+        data = pylab.loadtxt(self.fname)
+        self.wavelen = data[:,0]
+        self.lum = data[:,1]
+        self.flatfield_corrected = False
+        self.background_corrected = False
+        
     def remove_cosmic_rays( self, threshold=3.0, interpolate=False ):
         """
         This assumes that you've already loaded the file and have self.lum
